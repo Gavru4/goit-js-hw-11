03151e920ed7a8da08b218c07934d6b1cb862cb9
Roman Havryliuk <gavru4r@gmail.com>
2022-01-17 19:55:11 +0200

    create markup with foto from axios

--- src/index.js
+++ src/index.js
@@ -1,2 +1,51 @@
 import './sass/main.scss';
-import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import getImage from './js/data';
+
+// Notify.success('Sol lucet omnibus');
+// Notify.failure('Qui timide rogat docet negare');
+
+const form = document.querySelector('#search-form');
+const input = document.querySelector('[name="searchQuery"]');
+const gallery = document.querySelector('.gallery');
+
+form.addEventListener('submit', onInputValue);
+
+function onInputValue(e) {
+  e.preventDefault();
+
+  const inputValue = input.value;
+
+  getImage(inputValue).then(data => {
+    if (data.data.hits.length === 0) {
+      return Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.',
+      );
+    }
+    gallery.insertAdjacentHTML('afterbegin', renderMarkup(data));
+  });
+}
+
+function renderMarkup(data) {
+  const markup = data.data.hits
+    .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+      return `
+        <div class="photo-card">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        <div class="info">
+            <p class="info-item">
+                <b>Likes${likes}</b>
+            </p>
+                <p class="info-item">
+            <b>Views${views}</b>
+            </p>
+                <p class="info-item">
+            <b>Comments${comments}</b>
+                </p>
+            <p class="info-item">
+            <b>Downloads${downloads}</b>
+            </p></div></div>`;
+    })
+    .join('');
+  return markup;
+}
