e2d9638871132ad7ef9a1146f721d5d04fd23b01
Roman Havryliuk <gavru4r@gmail.com>
2022-01-18 18:17:25 +0200

    update file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './sass/main.scss';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import getImage from './js/data';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 // Notify.success('Sol lucet omnibus');
 // Notify.failure('Qui timide rogat docet negare');
@@ -8,12 +10,12 @@ import getImage from './js/data';
 const form = document.querySelector('#search-form');
 const input = document.querySelector('[name="searchQuery"]');
 const gallery = document.querySelector('.gallery');
+const loadBtn = document.querySelector('.load-more');
 
-form.addEventListener('submit', onInputValue);
+input.form.addEventListener('submit', onInputValue);
 
 function onInputValue(e) {
-  e.preventDefault();
-
+  e.preventDefault(getImage());
   const inputValue = input.value;
 
   getImage(inputValue).then(data => {
@@ -23,6 +25,9 @@ function onInputValue(e) {
       );
     }
     gallery.insertAdjacentHTML('afterbegin', renderMarkup(data));
+    lightbox.refresh();
+    loadBtn.classList.toggle('is-hidden');
+    return Notify.success(`"Hooray! We found ${data.data.totalHits} images."`);
   });
 }
 
@@ -30,22 +35,52 @@ function renderMarkup(data) {
   const markup = data.data.hits
     .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
       return `
+        <div class= gallery-wrap>
         <div class="photo-card">
-        <img src="${webformatURL}" alt="${tags}" loading="lazy" />
-        <div class="info">
-            <p class="info-item">
-                <b>Likes${likes}</b>
+          <a href="${largeImageURL}">
+            <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+          </a>
+          <div class="info">
+              <p class="info-item">
+                 <b>Likes<br>${likes}</b>
+              </p>
+              <p class="info-item">
+            <b>Views<br>${views}</b>
             </p>
                 <p class="info-item">
-            <b>Views${views}</b>
-            </p>
-                <p class="info-item">
-            <b>Comments${comments}</b>
+            <b>Comments<br>${comments}</b>
                 </p>
             <p class="info-item">
-            <b>Downloads${downloads}</b>
-            </p></div></div>`;
+            <b>Downloads<br>${downloads}</b>
+            </p>
+            </div>
+        </div>
+        </div>`;
     })
     .join('');
   return markup;
 }
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  swipeClose: true,
+  close: true,
+});
+
+loadBtn.addEventListener('click', onLoadBtnClick);
+
+function onLoadBtnClick() {
+  page += 1;
+  getImage(name, page).then(data => {
+    renderMarkup(data);
+  });
+}
+// onLoadBtnClick();
+
+// const { height: cardHeight } = document
+//   .querySelector('.gallery')
+//   .firstElementChild.getBoundingClientRect();
+
+// window.scrollBy({
+//   top: cardHeight * 2,
+//   behavior: 'smooth',
+// });
--- src/js/data.js
+++ src/js/data.js
@@ -4,15 +4,15 @@ import axios from 'axios';
 const KEY = '25285051-4b045d69a43564daa3e04547c';
 const BASE_URL = 'https://pixabay.com/api?';
 
-export default function getImage(name) {
+export default function getImage(name, page = 1) {
   try {
     const response = axios.get(
       `${BASE_URL}key=${KEY}&q=${name}
       &image_type=photo
-      &orientation=horizontal&safesearch=true&page=1&per_page=40
+      &orientation=horizontal&safesearch=true&page=${page}&per_page=40
 `,
     );
-    console.log(response);
+    // console.log(response);
     return response;
   } catch (error) {
     console.error(error);
